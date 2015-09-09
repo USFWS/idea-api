e1c9fd467ef06efefd3b184ab7d0bc2f9ffcd3e0
Roy Hewitt <roy_hewitt@fws.gov>
2015-09-09 14:13:21 -0400

    Notify users subscribed to tag of new idea

--- api/controllers/IdeaController.js
+++ api/controllers/IdeaController.js
@@ -6,38 +6,36 @@
  */
 var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
 var Promise = require('bluebird');
-var BadgeService = Promise.promisifyAll(require('../Services/BadgeService'));
 
 module.exports = {
 	create: function(req, res) {
     var data = actionUtil.parseValues(req),
       response = {};
+      data.creator = req.user.id;
 
     Idea.create(data)
       .then(function (idea) {
+        return Idea.findOne({ id: idea.id })
+          .populate('tags');
+      })
+      .then(function (idea) {
         response.idea = idea;
-        return User.findOne({ id: data.creator }).populate('ideas').populate('badges');
+        return User.findOne({ id: data.creator })
+          .populate('ideas')
+          .populate('badges');
       })
       .then(function (user) {
-        response.user = user;
-        return BadgeService.newIdea(user);
+        return Promise.join(
+          BadgeService.newIdea(user),
+          NotifyService.newIdea(response.idea)
+        );
       })
       .then(function (results) {
-        var innovator = results[0],
-          prolific = results[1];
-
-        if (innovator) {
-          response.user.badges.add(innovator[0]);
-          response.user.save();
-          response.badges = ['Innovator'];
-        } else if (prolific) {
-          response.user.badges.add(prolific[0]);
-          response.user.save();
-          response.badges = ['Prolific Innovator'];
-        }
+        response.badges = results[0];
+        response.notifications = results[1];
         return res.created(response);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         return res.negotiate(error);
       });
   }
